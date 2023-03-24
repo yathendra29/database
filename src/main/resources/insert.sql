bc59791f185ac5e829ebfc5f1252f0f83ecd0dda
yathendra29 <92430668+yathendra29@users.noreply.github.com>
2023-03-24 21:58:34 +0530

    initial commit

---
+++ src/main/resources/insert.sql
@@ -0,0 +1,3 @@
+insert into myfirstdb.employeeDetails values(104,'Ntr',39,'hyd',"2002-05-21"),
+                                            (105,'samantha',33,'chennai',"2010-07-29"),
+                                            (106,'salman',55,'mumbai',"1993-07-29")
